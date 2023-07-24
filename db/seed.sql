327f5c9637e6a10395283d9de59687b2c8fb2c0f
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-07-23 23:32:34 -0400

    CRUD Working

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Ya Ilahi', 'Omar Esa', 'Guide Me', '3:40', true),
+('Ya Aqsa', 'Muad', 'Aqsa', '3:18', true),
+('Alhamdulillah', 'Siedd', 'Journey', '2:20', false);
